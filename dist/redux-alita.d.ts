e3a2cf21e253f5d674e415763da80a6fb59a8bfd
chenghao <hao.cheng@karakal.com.cn>
2019-07-22 23:17:50 +0800

    update type file

--- dist/redux-alita.d.ts
+++ dist/redux-alita.d.ts
@@ -1,15 +1,50 @@
 // Type definitions for redux-alita
 // Project: https://github.com/yezihaohao/redux-alita
 // Definitions by: yezihaohao <https://github.com/yezihaohao>
+import * as React from 'react';
 
 interface AlitaParam {
-    funcName?: string,
-    params?: any,
-    stateName?: string,
-    data?: any
+    funcName?: string;
+    params?: any;
+    stateName?: string;
+    data?: any;
 }
+
+export class AlitaProvider extends React.Component {}
+
+/**
+ * connect component with redux
+ * @param alitaStateKeys state keys extract from redux
+ */
+export function connectAlita(alitaStateKeys: []): any;
+
 /**
  * function set redux state
- * @param param params for setting alita state
+ * @param params params for setting alita state
  */
-export function setAlitaState(param: AlitaParam): void;
+export function setAlitaState(params: AlitaParam): void;
+
+/**
+ * register api functions for async api request
+ * @param apis api collection
+ */
+export function setConfig(apis: object): void;
+
+/**
+ * set alita state in redux
+ * @return setAlitaState function
+ */
+export function useAlitaCreator(): () => void;
+
+/**
+ * get alita state from redux
+ * @return alita state from redux
+ * @example
+ * // get count from redux, without initial value,
+ * // default count is undefined
+ * const { count } = useAlitaState(['count']);
+ * // get count from redux, with initial value,
+ * // default count is 0
+ * const { count } = useAlitaState([{ count: 0 }]);
+ */
+export function useAlitaState(): any;
